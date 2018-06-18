df4db35fddf0704cc2760cf925a455792e72ed51
Timothy Jones <tjones@dius.com.au>
2018-06-18 16:46:18 +1000

    Remove scores scaffold

--- app/models/score.rb
+++
@@ -1,4 +0,0 @@
-class Score < ApplicationRecord
-  def as_json(options={})
-    { :timesReached => self.reachedby }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'scoreboard', to: 'score_summaries#see_score'
   post 'scoreboard', to: 'score_summaries#save_score'
   resources :score_summaries
-  resources :scores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
